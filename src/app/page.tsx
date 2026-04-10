3d9f0a884c45dc957c9df64340bc0bc3305f8f11
Omurbek1 <omurbekmamytbekov545@gmail.com>
2026-04-10 21:15:28 +0600

    f

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Box from "@component/Box";
+import Market1 from "./market-1/page";
+// PAGE SECTION COMPONENTS
+
+export default function Home() {
+  return (
+    <Box id="top" overflow="hidden" bg="gray.white">
+      <Market1 />
+    </Box>
+  );
+}
