9714030890d571fe21fb3a610e294772b93e19a0
Omurbek1 <omurbekmamytbekov545@gmail.com>
2026-04-11 09:23:29 +0600

    f

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing"; // Убедитесь, что путь правильный
+import { routing } from 'i18n/routing';
 
 export default createMiddleware(routing);
 
